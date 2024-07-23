303e83ab4c5e89bda566f07714d59da180f30578
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2024-07-23 16:03:53 -0400

    Improvements

--- proof-of-concept.jl
+++ proof-of-concept.jl
@@ -6,39 +6,23 @@ using Interpolations
 using DataFrames
 using Plots
 
-# Wrappers to different LU algorithms and implementations
+# # Smart discovery
+
+# Define wrappers to different LU algorithms and implementations
 function umfpack_a(A)
     t = @elapsed  L, U, p = lu(A)
     err = norm(A[p,:] - L*U, 1)
     return t, err
 end
-
 function klu_a(A)
     sA = @views sparse(A)
     t = @elapsed K = klu(sA)
     err = norm(K.L * K.U + K.F - K.Rs .\ A[K.p, K.q], 1)
     return t, err
 end
+algs = [umfpack_a, klu_a]
 
-# Define metrix names, sizes, and algorithms to explore
-
-# All matrices
-# mat_names =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
-#              "clement","companion","deriv2","dingdong","erdrey","fiedler",
-#              "forsythe","foxgood","frank","gilbert","golub","gravity","grcar",
-#              "hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms",
-#              "lehmer","lotkin","magic","minij","moler","neumann","oscillate",
-#              "parallax","parter","pascal","pei","phillips","poisson","prolate",
-#              "randcorr","rando","randsvd","rohess","rosser","sampling","shaw",
-#              "smallworld","spikes","toeplitz","tridiag","triw","ursell","vand",
-#              "wathen","wilkinson","wing"]
-
-# Problematic matrices: interface issues (e.g. dim^2 instead of dim), out of memory, singular value exception.
-# mat_names = ["binomial", "blur", "poisson", "chow", "erdrey", "invol","neumann",
-#              "parallax","pascal","rosser",,"vand", "smallworld","gilbert",
-#              "hadamard","heat","invhilb", "wathen", "chebspec","phillips",] 
-
-# Matrices
+# Define matrices
 mat_names = ["baart", "cauchy",  "circul", "clement", "companion", 
              "deriv2", "dingdong", "fiedler", "forsythe", "foxgood","frank",
              "golub","gravity","grcar", "hankel","hilb","kahan","kms", "lehmer",
@@ -47,13 +31,11 @@ mat_names = ["baart", "cauchy",  "circul", "clement", "companion",
              "sampling","shaw", "spikes","toeplitz","tridiag","triw","ursell",
              "wilkinson","wing"]
 
-# Matrix sizes
+# Define matrix sizes
 ns = [10, 100, 1000]
 
-# Algorithms
-algs = [umfpack_a, klu_a]
 
-# Evaluate
+# # Generate smart choice database trough smart discovery
 df = DataFrame(mat_name = String[], n = Int[], algorithm = String[], 
                time = Float64[], error = Float64[])
 for mat_name in mat_names
@@ -71,5 +53,40 @@ for mat_name in mat_names
     end
 end
 
+# # Show discovery process results
 df
+
+# TODO: Plot here
+
+# # Generate smart choice model
+
+#TODO: ML model here
+
+smart_choice = Dict()
+for mat_name in mat_names
+    for n in ns
+        df′ = @views df[(df.mat_name .== mat_name) .&& (df.n .== n), :]
+        min_time = minimum(df′.time)
+        min_time_row = df′[df′.time .== min_time, :]
+        push!(smart_choice, (mat_name, n) => eval(Meta.parse(min_time_row.algorithm[1])))
+    end
+end
+
+############
+
+# All matrices
+# mat_names =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
+#              "clement","companion","deriv2","dingdong","erdrey","fiedler",
+#              "forsythe","foxgood","frank","gilbert","golub","gravity","grcar",
+#              "hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms",
+#              "lehmer","lotkin","magic","minij","moler","neumann","oscillate",
+#              "parallax","parter","pascal","pei","phillips","poisson","prolate",
+#              "randcorr","rando","randsvd","rohess","rosser","sampling","shaw",
+#              "smallworld","spikes","toeplitz","tridiag","triw","ursell","vand",
+#              "wathen","wilkinson","wing"]
+
+# Problematic matrices: interface issues (e.g. dim^2 instead of dim), out of memory, singular value exception.
+# mat_names = ["binomial", "blur", "poisson", "chow", "erdrey", "invol","neumann",
+#              "parallax","pascal","rosser",,"vand", "smallworld","gilbert",
+#              "hadamard","heat","invhilb", "wathen", "chebspec","phillips",] 
 
