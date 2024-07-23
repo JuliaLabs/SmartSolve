e831a933d7393c670a2944eeee2ac884704a0394
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2024-07-23 13:24:26 -0400

    Add more matrices

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # SmartSolve. Proof of concept.
 
+Getting started
+
+```bash
+cd SmartSolve
+julia --project=.
+```
+
+```julia
+pkg> instantiate
+julia> include("proof-of-concept.jl")
+```
+
--- proof-of-concept.jl
+++ proof-of-concept.jl
@@ -22,20 +22,55 @@ end
 
 # Define metrix names, sizes, and algorithms to explore
 
-#mat_names = ["baart","binomial","blur","cauchy","chebspec","chow","circul","clement","companion","deriv2","dingdong","erdrey","fiedler","forsythe","foxgood","frank","gilbert","golub","gravity","grcar","hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms","lehmer","lotkin","magic","minij","moler","neumann","oscillate","parallax","parter","pascal","pei","phillips","poisson","prolate","randcorr","rando","randsvd","rohess","rosser","sampling","shaw","smallworld","spikes","toeplitz","tridiag","triw","ursell","vand","wathen","wilkinson","wing"]
-mat_names = ["baart"]
+# All matrices
+#mat_names =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
+#              "clement","companion","deriv2","dingdong","erdrey","fiedler",
+#              "forsythe","foxgood","frank","gilbert","golub","gravity","grcar",
+#              "hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms",
+#              "lehmer","lotkin","magic","minij","moler","neumann","oscillate",
+#              "parallax","parter","pascal","pei","phillips","poisson","prolate",
+#              "randcorr","rando","randsvd","rohess","rosser","sampling","shaw",
+#              "smallworld","spikes","toeplitz","tridiag","triw","ursell","vand",
+#              "wathen","wilkinson","wing"]
+
+# Problematic matrices: interface issues (e.g. dim^2 instead of dim), out of memory, singular value exception.
+# mat_names = ["binomial", "blur", "poisson", "chow", "erdrey", "invol","neumann",
+#              "parallax","pascal","rosser",,"vand", "smallworld","gilbert",
+#              "hadamard","heat","invhilb", "wathen"] 
+
+# Matrices
+mat_names = ["baart", "cauchy", "chebspec", "circul", "clement", "companion", 
+             "deriv2", "dingdong", "fiedler",
+             "forsythe", "foxgood","frank","golub","gravity","grcar",
+             "hankel","hilb","kahan","kms",
+             "lehmer", "lotkin","magic","minij","moler","oscillate",
+             "parter","pei","phillips","prolate",
+             "randcorr","rando","randsvd","rohess","sampling","shaw",
+             "spikes","toeplitz","tridiag","triw","ursell",
+             "wilkinson","wing"]
+# Matrix sizes
 ns = [900]
+
+# Algorithms
 algs = [umfpack_a, klu_a]
 
+# Evaluate
 df = DataFrame(mat_name = String[], n = Int[], algorithm = String[], 
                t = Float64[], err = Float64[])
 for mat_name in mat_names
     for n in ns
         A = matrixdepot(mat_name, n)
         for a in algs
-            t, err = a(A)
-            push!(df, [mat_name, n, "$(nameof(a))", t, err])
+            try
+                t, err = a(A)
+                push!(df, [mat_name, n, "$(nameof(a))", t, err])
+            catch e
+                println("$(mat_name),$n, $(nameof(a)): an error occurred: $e")
+            end
         end
+        GC.gc()
     end
 end
+
+df
 
