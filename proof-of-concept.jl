ab90a56a12d3d6465e5503ce71a6fae5cf065a46
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2024-07-30 18:02:19 -0400

    Progress in ML model.

--- proof-of-concept.jl
+++ proof-of-concept.jl
@@ -45,13 +45,13 @@ end
 algs = [umfpack_a, klu_a, splu_a]
 
 # Define matrices
-mat_names = ["rosser", "companion", "forsythe", "grcar", "triw", "blur", "poisson",
-             "heat", "kahan", "frank", "rohess", "baart", "cauchy", "circul",
-             "clement", "deriv2", "dingdong", "fiedler", "foxgood", "golub",
-             "gravity", "hankel","hilb", "kms", "lehmer", "lotkin","magic",
-             "minij", "moler","oscillate", "parter", "pei", "prolate", "randcorr",
-             "rando","randsvd","sampling","shaw", "spikes", "toeplitz",
-             "tridiag","ursell", "wilkinson","wing", "hadamard", "phillips"]
+mat_patterns = ["rosser", "companion", "forsythe", "grcar", "triw", "blur", "poisson",
+                "heat", "kahan", "frank", "rohess", "baart", "cauchy", "circul",
+                "clement", "deriv2", "dingdong", "fiedler", "foxgood", "golub",
+                "gravity", "hankel","hilb", "kms", "lehmer", "lotkin","magic",
+                "minij", "moler","oscillate", "parter", "pei", "prolate", "randcorr",
+                "rando","randsvd","sampling","shaw", "spikes", "toeplitz",
+                "tridiag","ursell", "wilkinson","wing", "hadamard", "phillips"]
 
 # Define matrix sizes
 #ns = [2^6, 2^8, 2^10, 2^12, 2^14]
@@ -61,29 +61,29 @@ ns = [2^6, 2^8, 2^10]
 n_experiments = 5
 
 # Generate smart choice database through smart discovery
-df = DataFrame(mat_name = String[], n = Int[], algorithm = String[], 
+df = DataFrame(mat_pattern = String[], n = Int[], algorithm = String[], 
                time = Float64[], error = Float64[])
 for i in 1:n_experiments
     println("Experiment $i")
-    for mat_name in mat_names
+    for mat_pattern in mat_patterns
         for n in ns
             # Generate matrix
-            if mat_name in ["blur", "poisson"]
+            if mat_pattern in ["blur", "poisson"]
                 n′ = convert(Int, sqrt(n))
             else
                 n′ = n
             end
-            A = matrixdepot(mat_name, n′)
+            A = matrixdepot(mat_pattern, n′)
             if size(A) != (n, n)
-                println("Check matrix size: $(mat_name), ($n, $n) vs $(size(A))")
+                println("Check matrix size: $(mat_pattern), ($n, $n) vs $(size(A))")
             end
             # Evaluate different algorithms
             for a in algs
                 try
                     t, err = a(A)
-                    push!(df, [mat_name, n, "$(nameof(a))", t, err])
+                    push!(df, [mat_pattern, n, "$(nameof(a))", t, err])
                 catch e
-                    println("$e. $(mat_name), $n, $(nameof(a))")
+                    println("$e. $(mat_pattern), $n, $(nameof(a))")
                 end
             end
             GC.gc()
@@ -97,14 +97,14 @@ end
 df
 CSV.write("smartsolve.csv", df)
 
-function plot_benchmark(df, ns, algs, mat_names, xaxis_type)
+function plot_benchmark(df, ns, algs, mat_patterns, xaxis_type)
     algs_str = ["$(nameof(a))" for a in algs]
     for n in ns
         p = plot(
             [(
                 ts = [
                        (
-                           df′ = @views df[(df.mat_name .== mat_name) .&&
+                           df′ = @views df[(df.mat_pattern .== mat_pattern) .&&
                                            (df.n .== n) .&& 
                                            (df.algorithm .== a), :];
                            if length(df′.time) > 0
@@ -113,9 +113,9 @@ function plot_benchmark(df, ns, algs, mat_names, xaxis_type)
                               0.0
                            end
                        )
-                       for mat_name in reverse(mat_names)
+                       for mat_pattern in reverse(mat_patterns)
                      ];
-                 bar(name=a, x=ts, y=reverse(mat_names), orientation="h")
+                 bar(name=a, x=ts, y=reverse(mat_patterns), orientation="h")
                 ) for a in algs_str
              ])
         relayout!(p, barmode="group",
@@ -126,33 +126,118 @@ function plot_benchmark(df, ns, algs, mat_names, xaxis_type)
     end
 end
 
-plot_benchmark(df, ns, algs, mat_names, "log")
-plot_benchmark(df, ns, algs, mat_names[1:11], "linear")
+plot_benchmark(df, ns, algs, mat_patterns, "log")
+plot_benchmark(df, ns, algs, mat_patterns[1:11], "linear")
 
-#for mat_name in mat_names[1:11]
-#    A = sparse(matrixdepot(mat_name, 2^6))
+#for mat_pattern in mat_patterns[1:11]
+#    A = sparse(matrixdepot(mat_pattern, 2^6))
 #    println(A)
 #end 
 
 # Generate smart choice model ##################################################
 
 ##TODO: ML model here
+#using Flux
+#using Images: imresize
+#using Statistics
+
+#n_patterns = length(mat_patterns)
+#n_cnn = 28
+
+#cnn = Flux.@autosize (n_cnn, n_cnn, 1, 1) Chain(
+#    Conv((5, 5), 1=>6, sigmoid),
+#    MaxPool((2, 2)),
+#    Conv((5, 5), _=>16, sigmoid),
+#    MaxPool((2, 2)),
+#    Flux.flatten,
+#    Dense(_ => 120, sigmoid),
+#    Dense(_ => 84, sigmoid), 
+#    Dense(_ => n_patterns),
+#) |> gpu
+
+## Loss function
+#loss(x, y) = Flux.logitbinarycrossentropy(x, y)
+##loss(x, y) = Flux.mse(x, y)
+
+## Define optimization state object
+#eta = 3e-4     # learning rate
+#lambda = 1e-2  # for weight decay
+#opt_rule = OptimiserChain(WeightDecay(lambda), Adam(eta))
+#opt_state = Flux.setup(opt_rule, cnn)
+
+## Create a dictionary to map patterns to indices
+#pattern_to_index = Dict(pattern => i for (i, pattern) in enumerate(mat_patterns))
+
+## Training 
+#for epoch in 1:100
+#    # Create batch
+#    batch_size = 32
+#    
+#    ns_batch = rand(ns, batch_size)
+#    
+#    mat_patterns_batch = rand(mat_patterns, batch_size)
+#    mat_pattern_indices = [pattern_to_index[mat_pattern] for mat_pattern in mat_patterns_batch]
+#    onehot_encoded = Flux.onehotbatch(mat_pattern_indices, 1:length(mat_patterns))
+#    
+#    As = []
+#    for j in 1:batch_size
+#        # Generate matrix
+#        mat_pattern = mat_patterns_batch[j]
+#        n = ns_batch[j]
+#        if mat_pattern in ["blur", "poisson"]
+#            n′ = convert(Int, sqrt(n))
+#        else
+#            n′ = n
+#        end
+#        A = matrixdepot(mat_pattern, n′)
+#        if size(A) != (n, n)
+#            println("Check matrix size: $(mat_pattern), ($n, $n) vs $(size(A))")
+#        end
+#        push!(As, imresize(A, (n_cnn, n_cnn), method=Linear()))
+#    end
+#    A′ = cat(As..., dims=4)
+
+#    grads = Flux.gradient(cnn -> loss(cnn(A′), onehot_encoded), cnn)
+#    Flux.update!(opt_state, cnn, grads[1])
+
+#    # Checks
+#    ŷ = cnn(A′)
+#    y = onehot_encoded
+#    train_batch_loss = loss(ŷ, y)
+#    train_bacth_acc = round(100 * mean(Flux.onecold(ŷ) .== Flux.onecold(y)); digits=2)
+#    println("Epoch: $epoch, training batch loss: $(train_batch_loss), training batch acc: $train_bacth_acc")
+
+#end
+
+## Predict
+#function predict(A, cnn)
+#    A′ = imresize(A, (n_cnn, n_cnn))
+#    A′ = reshape(A′, (n_cnn, n_cnn, 1, 1), method=Linear())
+#    return cnn(A′)
+#end
+
+#rand_pattern = rand(mat_patterns)
+#rand_n = ns_batch = rand(ns)
+#A = matrixdepot(rand_pattern, rand_n)
+#pattern_to_index[rand_pattern]
+#predict(A, cnn)
+#plot(reshape(predict(A, cnn), (46)))
 
 
 smart_choice = Dict()
-for mat_name in mat_names
+for mat_pattern in mat_patterns
     for n in ns
-        df′ = @views df[(df.mat_name .== mat_name) .&& (df.n .== n), :]
+        df′ = @views df[(df.mat_name .== mat_pattern) .&& (df.n .== n), :]
         min_time = minimum(df′.time)
         min_time_row = df′[df′.time .== min_time, :]
-        push!(smart_choice, (mat_name, n) => eval(Meta.parse(min_time_row.algorithm[1])))
+        push!(smart_choice, (mat_pattern, n) => eval(Meta.parse(min_time_row.algorithm[1])))
     end
 end
 
 ################################################################################
 
 # All matrices
-# mat_names =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
+# mat_patterns =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
 #              "clement","companion","deriv2","dingdong","erdrey","fiedler",
 #              "forsythe","foxgood","frank","gilbert","golub","gravity","grcar",
 #              "hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms",
@@ -161,9 +246,9 @@ end
 #              "randcorr","rando","randsvd","rohess","rosser","sampling","shaw",
 #              "smallworld","spikes","toeplitz","tridiag","triw","ursell","vand",
 #              "wathen","wilkinson","wing"]
-#mat_names = ["chow", "erdrey", "invol", "neumann", "parallax", "pascal", "vand",
+#mat_patterns = ["chow", "erdrey", "invol", "neumann", "parallax", "pascal", "vand",
 #             "smallworld", "gilbert", "chebspec"] # singular exception
-#mat_names = ["binomial", "wathen", "invhilb"] # overflow?
+#mat_patterns = ["binomial", "wathen", "invhilb"] # overflow?
 
 
 #A = matrixdepot("blur", 2^5)
@@ -184,40 +269,5 @@ end
 #x = res \ b
 #err = norm(A * x - b, 1)
 
-#using Flux
 
-#n_algs = 2 #length(algs)
-#m = Chain(Conv((3,3), 1 => n_algs), GlobalMaxPool());
-#n = 100
-#m(rand32(n, n, 1, 1))
-
-## Loss function and optimizer
-#loss(x, y) = Flux.crossentropy(model(x), y)
-#opt = ADAM()
-
-#y_onehot = Flux.onehotbatch(algs, 0:1)
-
-#for epoch in 1:10
-#    for mat_name in mat_names
-#        for n in ns
-#            # Generate matrix
-#            if mat_name in ["blur", "poisson"]
-#                n′ = convert(Int, sqrt(n))
-#            else
-#                n′ = n
-#            end
-#            A = matrixdepot(mat_name, n′)
-#            if size(A) != (n, n)
-#                println("Check matrix size: $(mat_name), ($n, $n) vs $(size(A))")
-#            end
-
-#            grads = Flux.gradient(() -> loss(x, y), Flux.params(model))
-#            Flux.Optimise.update!(opt, Flux.params(model), grads)
-#        end
-#    end
-#        
-#    # Calculate validation loss
-#    val_loss = mean([loss(x, y) for (x, y) in val_loader])
-#    println("Epoch: $epoch, Validation Loss: $val_loss")
-#end
 
