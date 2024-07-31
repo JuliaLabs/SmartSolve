df7a7d8678060ceade2ff9224874c157ad6fea4e
Emmanuel Lujan <lujan.emmanuel@gmail.com>
2024-07-30 21:23:31 -0400

    Progress in ML model.

--- proof-of-concept.jl
+++ proof-of-concept.jl
@@ -137,6 +137,149 @@ plot_benchmark(df, ns, algs, mat_patterns[1:11], "linear")
 # Generate smart choice model ##################################################
 
 ##TODO: ML model here
+using Flux
+using Images: imresize
+using Statistics
+
+n_patterns = length(mat_patterns)
+n_cnn = 64
+
+cnn = Flux.@autosize (n_cnn, n_cnn, 1, 1) Chain(
+    Conv((5, 5), 1=>6, relu),
+    MaxPool((2, 2)),
+    Conv((5, 5), _=>16, relu),
+    MaxPool((2, 2)),
+    Flux.flatten,
+    Dense(_ => 120, relu),
+    Dense(_ => 84, relu), 
+    Dense(_ => n_patterns),
+) |> gpu
+
+# Loss function
+loss(x, y) = Flux.logitbinarycrossentropy(x, y)
+
+# Define optimization state object
+eta = 3e-4     # learning rate
+lambda = 1e-2  # for weight decay
+opt_rule = OptimiserChain(WeightDecay(lambda), Adam(eta))
+opt_state = Flux.setup(opt_rule, cnn)
+
+# Create a dictionary to map patterns to indices
+pattern_to_index = Dict(pattern => i for (i, pattern) in enumerate(mat_patterns))
+
+
+ns = [2^6, 2^8, 2^10]
+
+# Training 
+for epoch in 1:1000
+    # Create batch
+    batch_size = 1024
+    
+    ns_batch = rand(ns, batch_size)
+    
+    mat_patterns_batch = rand(mat_patterns, batch_size)
+    mat_pattern_indices = [pattern_to_index[mat_pattern] for mat_pattern in mat_patterns_batch]
+    onehot_encoded = Flux.onehotbatch(mat_pattern_indices, 1:length(mat_patterns))
+    
+    As = []
+    for j in 1:batch_size
+        # Generate matrix
+        mat_pattern = mat_patterns_batch[j]
+        n = ns_batch[j]
+        if mat_pattern in ["blur", "poisson"]
+            n′ = convert(Int, sqrt(n))
+        else
+            n′ = n
+        end
+        A = matrixdepot(mat_pattern, n′)
+        if size(A) != (n, n)
+            println("Check matrix size: $(mat_pattern), ($n, $n) vs $(size(A))")
+        end
+
+        row_start = rand(1:n-n_cnn+1)
+        col_start = rand(1:n-n_cnn+1)
+        A = A[row_start:row_start+n_cnn-1, col_start:col_start+n_cnn-1]
+
+        #push!(As, imresize(A, (n_cnn, n_cnn), method=Linear()))
+        push!(As, A)
+    end
+    A′ = cat(As..., dims=4)
+
+    grads = Flux.gradient(cnn -> loss(cnn(A′), onehot_encoded), cnn)
+    Flux.update!(opt_state, cnn, grads[1])
+
+    # Checks
+    ŷ = cnn(A′)
+    y = onehot_encoded
+    train_batch_loss = loss(ŷ, y)
+    train_bacth_acc = round(100 * mean(Flux.onecold(ŷ) .== Flux.onecold(y)); digits=2)
+    println("Epoch: $epoch, training batch loss: $(train_batch_loss), training batch acc: $train_bacth_acc")
+
+end
+
+# Predict
+function predict(A, cnn)
+    A′ = imresize(A, (n_cnn, n_cnn))
+    A′ = reshape(A′, (n_cnn, n_cnn, 1, 1), method=Linear())
+    return cnn(A′)
+end
+
+#rand_pattern = rand(mat_patterns)
+#rand_n = ns_batch = rand(ns)
+#A = matrixdepot(rand_pattern, rand_n)
+#pattern_to_index[rand_pattern]
+#predict(A, cnn)
+#plot(reshape(predict(A, cnn), (46)))
+
+
+smart_choice = Dict()
+for mat_pattern in mat_patterns
+    for n in ns
+        df′ = @views df[(df.mat_name .== mat_pattern) .&& (df.n .== n), :]
+        min_time = minimum(df′.time)
+        min_time_row = df′[df′.time .== min_time, :]
+        push!(smart_choice, (mat_pattern, n) => eval(Meta.parse(min_time_row.algorithm[1])))
+    end
+end
+
+################################################################################
+
+# All matrices
+# mat_patterns =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
+#              "clement","companion","deriv2","dingdong","erdrey","fiedler",
+#              "forsythe","foxgood","frank","gilbert","golub","gravity","grcar",
+#              "hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms",
+#              "lehmer","lotkin","magic","minij","moler","neumann","oscillate",
+#              "parallax","parter","pascal","pei","phillips","poisson","prolate",
+#              "randcorr","rando","randsvd","rohess","rosser","sampling","shaw",
+#              "smallworld","spikes","toeplitz","tridiag","triw","ursell","vand",
+#              "wathen","wilkinson","wing"]
+#mat_patterns = ["chow", "erdrey", "invol", "neumann", "parallax", "pascal", "vand",
+#             "smallworld", "gilbert", "chebspec"] # singular exception
+#mat_patterns = ["binomial", "wathen", "invhilb"] # overflow?
+
+
+#A = matrixdepot("blur", 2^5)
+#b = rand(2^10)
+#t = @elapsed res = lu(Matrix(A))
+#x = res \ b
+#err = norm(A * x - b, 1)
+#t = @elapsed res = lu(A)
+#x = res \ b
+#err = norm(A * x - b, 1)
+
+#A = matrixdepot("rosser", 2^10)
+#b = rand(2^10)
+#t = @elapsed res = klu(sparse(A))
+#x = res \ b
+#err = norm(A * x - b, 1)
+#t = @elapsed res = klu(A)
+#x = res \ b
+#err = norm(A * x - b, 1)
+
+
+###############################################################################
+
 #using Flux
 #using Images: imresize
 #using Statistics
@@ -224,50 +367,13 @@ plot_benchmark(df, ns, algs, mat_patterns[1:11], "linear")
 #plot(reshape(predict(A, cnn), (46)))
 
 
-smart_choice = Dict()
-for mat_pattern in mat_patterns
-    for n in ns
-        df′ = @views df[(df.mat_name .== mat_pattern) .&& (df.n .== n), :]
-        min_time = minimum(df′.time)
-        min_time_row = df′[df′.time .== min_time, :]
-        push!(smart_choice, (mat_pattern, n) => eval(Meta.parse(min_time_row.algorithm[1])))
-    end
-end
-
-################################################################################
-
-# All matrices
-# mat_patterns =  ["baart","binomial","blur","cauchy","chebspec","chow","circul",
-#              "clement","companion","deriv2","dingdong","erdrey","fiedler",
-#              "forsythe","foxgood","frank","gilbert","golub","gravity","grcar",
-#              "hadamard","hankel","heat","hilb","invhilb","invol","kahan","kms",
-#              "lehmer","lotkin","magic","minij","moler","neumann","oscillate",
-#              "parallax","parter","pascal","pei","phillips","poisson","prolate",
-#              "randcorr","rando","randsvd","rohess","rosser","sampling","shaw",
-#              "smallworld","spikes","toeplitz","tridiag","triw","ursell","vand",
-#              "wathen","wilkinson","wing"]
-#mat_patterns = ["chow", "erdrey", "invol", "neumann", "parallax", "pascal", "vand",
-#             "smallworld", "gilbert", "chebspec"] # singular exception
-#mat_patterns = ["binomial", "wathen", "invhilb"] # overflow?
-
-
-#A = matrixdepot("blur", 2^5)
-#b = rand(2^10)
-#t = @elapsed res = lu(Matrix(A))
-#x = res \ b
-#err = norm(A * x - b, 1)
-#t = @elapsed res = lu(A)
-#x = res \ b
-#err = norm(A * x - b, 1)
-
-#A = matrixdepot("rosser", 2^10)
-#b = rand(2^10)
-#t = @elapsed res = klu(sparse(A))
-#x = res \ b
-#err = norm(A * x - b, 1)
-#t = @elapsed res = klu(A)
-#x = res \ b
-#err = norm(A * x - b, 1)
-
-
+#smart_choice = Dict()
+#for mat_pattern in mat_patterns
+#    for n in ns
+#        df′ = @views df[(df.mat_name .== mat_pattern) .&& (df.n .== n), :]
+#        min_time = minimum(df′.time)
+#        min_time_row = df′[df′.time .== min_time, :]
+#        push!(smart_choice, (mat_pattern, n) => eval(Meta.parse(min_time_row.algorithm[1])))
+#    end
+#end
 
